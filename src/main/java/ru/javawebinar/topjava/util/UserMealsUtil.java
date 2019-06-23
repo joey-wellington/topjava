be1363d03aba1aa4946ce1db8c778624810fb808
iko <third_thunder@mail.ru>
2019-06-24 00:36:08 +0300

    HW0 with loops

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,11 +3,11 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -19,13 +19,33 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
         );
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
-//        .toLocalDate();
-//        .toLocalTime();
+
+        List<UserMealWithExceed> filteredMeals =  getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+        filteredMeals.forEach(System.out::println);
+
     }
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with correctly exceeded field
-        return null;
+        Map<LocalDate, Integer> groupedMeals = new HashMap<>();
+        for (UserMeal meal : mealList) {
+            LocalDate date = meal.getDateTime().toLocalDate();
+            groupedMeals.put(date, groupedMeals.getOrDefault(date, 0) + meal.getCalories());
+        }
+        List<UserMealWithExceed> filteredMeals = new ArrayList<>();
+        for (UserMeal meal : mealList) {
+            if (isBetweenTimes(meal.getDateTime().toLocalTime(), startTime, endTime)) {
+                filteredMeals.add(new UserMealWithExceed(
+                        meal.getDateTime(),
+                        meal.getDescription(),
+                        meal.getCalories(),
+                        groupedMeals.get(meal.getDateTime().toLocalDate()) > caloriesPerDay
+                ));
+            }
+        }
+        return filteredMeals;
+    }
+
+    public static boolean isBetweenTimes(LocalTime time, LocalTime startTime, LocalTime endtime) {
+        return time.compareTo(startTime) >= 0 && time.compareTo(endtime) <= 0;
     }
 }
